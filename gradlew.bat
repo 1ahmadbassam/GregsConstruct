6777c20628fb7f80acb89e0f5f37ba07e808304c
Ahmad Bassam El Bizri <34858431+1ahmadbassam@users.noreply.github.com>
2024-05-11 18:48:38 +0300

    Update to GT 1.17

--- gradlew.bat
+++ gradlew.bat
@@ -32,7 +32,7 @@ echo location of your Java installation.
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-8.0.392.8-hotspot
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto init
